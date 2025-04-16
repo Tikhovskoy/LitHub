e11a80ae72f410c65b08b3f5519cf3ba269dbc59
Tikhovskoy <tihovskoyviktor@icloud.com>
2025-04-16 12:33:11 +0300

    Split books by two columns

--- live_server.py
+++ live_server.py
@@ -1,6 +1,7 @@
 import json
 from livereload import Server
 from jinja2 import Environment, FileSystemLoader, select_autoescape
+from more_itertools import chunked
 
 def on_reload():
     with open('meta_data.json', encoding='utf-8') as f:
@@ -8,13 +9,15 @@ def on_reload():
 
     books = books[:100]
 
+    book_pairs = list(chunked(books, 2))
+
     env = Environment(
         loader=FileSystemLoader('templates'),
         autoescape=select_autoescape(['html', 'xml'])
     )
     template = env.get_template('index.html')
 
-    rendered_page = template.render(books=books)
+    rendered_page = template.render(book_pairs=book_pairs)
 
     with open('index.html', 'w', encoding='utf-8') as f:
         f.write(rendered_page)
@@ -24,5 +27,4 @@ on_reload()
 server = Server()
 server.watch('templates/*.html', on_reload)
 server.watch('meta_data.json', on_reload)
-
 server.serve(root='.')
